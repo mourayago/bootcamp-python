a3bee00e032d85b62b31d4f701f81d36aa816948
mourayago <moura.yagoibmec@gmail.com>
2024-07-27 17:30:52 -0300

    subindo os arquivos iniciais do desafio de classes da aula 13

---
+++ aula_11-15/aula_13/src/libs/read_file_class.py
@@ -0,0 +1,16 @@
+
+
+# class FilesProcessor():
+#     def __init__(self, folder_path):
+#         self.folder_path = folder_path
+
+#     def get_files(self):
+#         '''Passa um caminho de um diretório e retorna uma lista com todos os arquivos contidos'''
+#         arr = os.listdir(self.folder_path)
+#         return arr
+    
+
+if __name__ == "__main__":
+    print('oi')
+    # obj = FilesProcessor("C:\\Users\\Yago\\Documents\\Bootcamp Python\\bootcamp-python\\aula_11-15\\aula_13\\data\\json_files")
+    # print(obj.get_files())
